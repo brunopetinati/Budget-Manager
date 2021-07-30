abd8b3f7e1b5ad066de7c3db359cfc0ec6c8aa11
Bruno Petinati <brunopetinati@hotmail.com>
2021-07-29 23:50:48 -0300

    tests implemented

--- tests_.py
+++ tests_.py
@@ -6,7 +6,7 @@ class TestAccountView(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        self.create_user_1 = { 
+        self.create_user = { 
         "username": "user1",
         "first_name":"User", 
         "last_name":"Last Name", 
@@ -18,7 +18,7 @@ class TestAccountView(TestCase):
         "password":"1234"
         }
 
-        self.user1_login = {
+        self.user_login = {
 	    "email":"userlastname@email.com",
 	    "password":"1234"
         }
@@ -26,7 +26,7 @@ class TestAccountView(TestCase):
     def test_create_and_login_for_user_account(self):
         # create user
         user = self.client.post(
-            "/api/accounts/", self.create_user_1, format="json"
+            "/api/accounts/", self.create_user, format="json"
         )
 
         # testa se o user foi criado corretamente
@@ -48,7 +48,52 @@ class TestAccountView(TestCase):
 
         # testa se o login foi realizado corretamente e se o token é retornado
         response = self.client.post(
-            "/api/login/", self.user1_login, format="json"
+            "/api/login/", self.user_login, format="json"
         ).json()
 
         self.assertIn("token", response.keys())
+
+class TestInfrastructureView(TestCase):
+    def setUp(self):
+
+        self.create_user = { 
+        "username": "user1",
+        "first_name":"User", 
+        "last_name":"Last Name", 
+        "full_name":"User Last Name",
+        "email":"userlastname@email.com",
+        "date_of_birth":"1990-12-12",
+        "CPF":"35555555555",
+        "wallet": 100,
+        "password":"1234"
+        }
+
+        self.user_login = {
+	    "email":"userlastname@email.com",
+	    "password":"1234"
+        }
+
+        self.transaction_data = {
+        "description":"Teste",
+        "transaction_type":"credit",
+        "amount": "20.00"
+    }
+
+    def test_admin_create_transaction(self):
+        client = APIClient()
+
+        user = client.post("/api/accounts/", self.create_user, format="json").json()
+
+        token = client.post("/api/login/", self.user_login, format="json").json()[
+            "token"
+        ]
+
+        client.credentials(HTTP_AUTHORIZATION="Token " + token)
+
+        transaction = client.post("/api/transactions/", self.transaction_data, format="json").json()
+
+        self.assertEqual(transaction["id"], 1)
+        self.assertEqual(transaction["transaction_type"], "credit")
+        self.assertEqual(transaction["amount"], '20.00')
+
+
